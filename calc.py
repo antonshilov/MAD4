5f27fe82493c923b0aeea8117a888b34b94427fd
vaccum121 <vaccum121@gmail.com>
2016-04-17 22:31:43 +0700

    minor updates

--- calc.py
+++ calc.py
@@ -1,13 +1,13 @@
 from math import sqrt
 from random import random
 
-model_types = {0: "a1+a2*(u1-u^)"}
+model_types = {0: "a1+a2*(u1-u_)"}
 
 
 class Sample:
     input_values = [[]]
     true_values = []
-    output_values = []
+    object_values = []
 
 
 def calc(interval, iterations_amount, hidrance_disp, function, var_num):
@@ -19,7 +19,6 @@ def calc(interval, iterations_amount, hidrance_disp, function, var_num):
 def generate_sample(interval, iterations_amount, hidrance_disp, function, var_num):
     step = interval / iterations_amount
     sample = Sample()
-    # sample.input_values
     param_value = 0
     input_values = []
     for i in range(iterations_amount):
@@ -29,7 +28,7 @@ def generate_sample(interval, iterations_amount, hidrance_disp, function, var_nu
             input_values[i].append(param_value)
             params.append(param_value)
         sample.true_values.append(calc_function(input_values[i], function))
-        sample.output_values.append(calc_function(params, function) + interference(hidrance_disp))
+        sample.object_values.append(sample.true_values[i] + interference(hidrance_disp))
         param_value += step
     sample.input_values = input_values
     return sample
@@ -37,14 +36,14 @@ def generate_sample(interval, iterations_amount, hidrance_disp, function, var_nu
 
 def build_model(sample, model_type):
     if model_type == 0:
-        a1 = sum(sample.output_values) / len(sample.output_values)
+        a1 = sum(sample.object_values) / len(sample.object_values)
         u_ = 0
         for val in sample.input_values:
             u_ += val[0]
         u_ /= len(sample.input_values)
         sm = 0
         for i in range(len(sample.input_values)):
-            sm += (sample.input_values[i][0] - u_) * sample.output_values[i]
+            sm += (sample.input_values[i][0] - u_) * sample.object_values[i]
         smq = 0
         for i in range(len(sample.input_values)):
             smq += (sample.input_values[i][0] - u_) ** 2
@@ -72,4 +71,4 @@ def calc_function(vals, f):
     return eval(f)
 
 
-calc(300, 200, 8, '4+2*(u1-6)', 1)
+calc(10, 100, 8, '4+6*(u1-1)', 1)
